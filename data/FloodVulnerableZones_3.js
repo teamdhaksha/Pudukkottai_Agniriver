093950997c8339df41d5a4dc72fc246d9066afca
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-11 10:14:19 +0530

    Add files via upload

---
+++ data/FloodVulnerableZones_3.js
@@ -0,0 +1,8 @@
+var json_FloodVulnerableZones_3 = {
+"type": "FeatureCollection",
+"name": "FloodVulnerableZones_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 79.020853341827902, 10.450399627751466 ] } }
+]
+}
---
+++ data/Towns_5.js
@@ -0,0 +1,9 @@
+var json_Towns_5 = {
+"type": "FeatureCollection",
+"name": "Towns_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Manjakarai" }, "geometry": { "type": "Point", "coordinates": [ 79.007216112792946, 10.45192529500595, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thuvar" }, "geometry": { "type": "Point", "coordinates": [ 79.054144041220468, 10.452395832646809, -999999.0 ] } }
+]
+}
